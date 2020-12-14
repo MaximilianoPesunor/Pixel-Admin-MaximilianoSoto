c301398ab4372870533c6fffcca8e84eb2b05d7e
MaximilianoPesunor <70050888+MaximilianoPesunor@users.noreply.github.com>
2020-12-14 01:07:50 -0300

    hola
    Primer Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlankComponent } from './componentes/blank/blank.component';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { ErrorComponent } from './componentes/error/error.component';
+import { GoogleComponent } from './componentes/google/google.component';
+import { IconosComponent } from './componentes/iconos/iconos.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { TablasComponent } from './componentes/tablas/tablas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'dashboard', component: DashboardComponent },
+{path: 'Perfil', component: PerfilComponent },
+{path: 'Tabla', component: TablasComponent },
+{path: 'Iconos', component: IconosComponent },
+{path: 'GoogleMap', component: GoogleComponent },
+{path: 'Blank', component: BlankComponent},
+{path: '', redirectTo: '/Perfil', pathMatch: 'full' },
+{path: 'Error', component: ErrorComponent },
+
+
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { BlankComponent } from './componentes/blank/blank.component';
+import { ErrorComponent } from './componentes/error/error.component';
+import { TablasComponent } from './componentes/tablas/tablas.component';
+import { IconosComponent } from './componentes/iconos/iconos.component';
+import { GoogleComponent } from './componentes/google/google.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    DashboardComponent,
+    PerfilComponent,
+    BlankComponent,
+    ErrorComponent,
+    TablasComponent,
+    IconosComponent,
+    GoogleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
